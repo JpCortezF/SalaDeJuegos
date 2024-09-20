f18aec15c574074a555c27f7bfd02e0e23bc0947
JpCortezF <juanpactz@gmail.com>
2024-09-20 00:45:44 -0300

    Sprint 2

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  isChatVisible = false;
 
+  ToggleChat() {
+    this.isChatVisible = !this.isChatVisible;
+
+    const chatIcon = document.getElementById('chatIcon')?.querySelector('img');
+    
+    if (this.isChatVisible) {
+      if (chatIcon) {
+        chatIcon.style.display = 'none';
+      }
+    } else {
+      if (chatIcon) {
+        chatIcon.style.display = 'block';
+        chatIcon.style.width = '60px';
+      }
+    }
+  }
 }
